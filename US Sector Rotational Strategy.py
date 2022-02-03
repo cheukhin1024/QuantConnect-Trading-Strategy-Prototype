9cc8ba1c53bcc394d2e9e6b44283ec8c6d720de2
Christophe <70860455+cheukhin1024@users.noreply.github.com>
2022-02-03 10:49:17 +0800

    Create US Sector Rotational Strategy.py

---
+++ US Sector Rotational Strategy.py
@@ -0,0 +1,52 @@
+#Please run the following code on QuantConnect
+
+#Don't naviely run the strategy and believe this can make money. No one will share the robust and profitable strategy. No one
+
+import pandas as pd
+from datetime import datetime
+
+class SectorMomentumAlgorithm(QCAlgorithm):
+
+    def Initialize(self):
+
+        self.SetStartDate(2005, 1, 1)  
+        self.SetEndDate(datetime.now())  
+        self.SetCash(100000) 
+        # create a dictionary to store momentum indicators for all symbols 
+        self.data = {}
+        period = 252
+        # choose ten sector ETFs
+        self.symbols = ["XLB",  # Vanguard Real Estate Index Fund
+                        "XLV",  # Technology Select Sector SPDR Fund
+                        "XLC",  # Energy Select Sector SPDR Fund
+                        "XLK",  # Health Care Select Sector SPDR Fund
+                        "XLF",  # Financial Select Sector SPDR Fund
+                        "XLP",  # SPDR S&P Bank ETF
+                        "XLI",  # Vanguard Materials ETF
+                        "XLU",  # Consumer Discretionary Select Sector SPDR Fund
+                        "XLY",  # Consumer Staples Select Sector SPDR Fund
+                        "XLE",
+                        "XLRE"]  # Vanguard Information Technology ETF
+                        
+        # warm up the MOM indicator
+        self.SetWarmUp(period)
+        for symbol in self.symbols:
+            self.AddEquity(symbol, Resolution.Daily)
+            self.data[symbol] = self.ROC(symbol, period, Resolution.Daily)
+        # shcedule the function to fire at the month start 
+        self.Schedule.On(self.DateRules.MonthStart("XLK"), self.TimeRules.AfterMarketOpen("XLK"), self.Rebalance)
+            
+    def OnData(self, data):
+        pass
+
+    def Rebalance(self):
+        if self.IsWarmingUp: return
+        top3 = pd.Series(self.data).sort_values(ascending = False)[:1]
+        for kvp in self.Portfolio:
+            security_hold = kvp.Value
+            # liquidate the security which is no longer in the top3 momentum list
+            if security_hold.Invested and (security_hold.Symbol.Value not in top3.index):
+                self.Liquidate(security_hold.Symbol)
+        
+        for symbol in top3.index:
+            self.SetHoldings(symbol, 1/len(top3))
